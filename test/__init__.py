7bb229eabcc2503f1b022a496fc566b8d8562692
Åke Forslund <ake.forslund@gmail.com>
2018-05-22 13:20:38 +0200

    Update mocking method to a working state

--- test/__init__.py
+++ test/__init__.py
@@ -3,11 +3,12 @@ from test.integrationtests.skills.skill_tester import SkillTest
 import mock
 
 @mock.patch('mycroft.skills.core.DeviceApi.send_email')
-@mock.patch('skill-support__init__.check_output')
-def test_runner(skill, example, emitter, loader, m1, m2):
+def test_runner(skill, example, emitter, loader, m1):
     def side_effect(title, body, skill):
         print("Sending e-mail")
 
-    m2.side_effect = side_effect
-    m1.return_value = "Test"
-    return SkillTest(skill, example, emitter).run(loader)
+    m1.side_effect = side_effect
+    s = [s for s in loader.skills if s and s.root_dir == skill][0]
+    with mock.patch(s.__module__ + '.check_output') as m2:
+        m2.return_value = "Test"
+        return SkillTest(skill, example, emitter).run(loader)
