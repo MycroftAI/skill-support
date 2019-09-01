216c6bad37c5a269ad504881d8714692bfc4724f
Åke Forslund <ake.forslund@gmail.com>
2019-09-01 10:31:20 +0200

    Fix mock of DeviceApi

--- test/__init__.py
+++ test/__init__.py
@@ -2,7 +2,8 @@ from test.integrationtests.skills.skill_tester import SkillTest
 
 import mock
 
-@mock.patch('mycroft.skills.core.DeviceApi.send_email')
+
+@mock.patch('mycroft.skills.mycroft_skill.mycroft_skill.DeviceApi.send_email')
 def test_runner(skill, example, emitter, loader, m1):
     def side_effect(title, body, skill):
         print("Sending e-mail")
