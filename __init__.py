944e9d5c10c93e3127ee65a5e4f031dad9c9ceb1
Kris Gesling <kris.gesling@mycroft.ai>
2021-01-21 14:42:16 +0930

    auto format

--- __init__.py
+++ __init__.py
@@ -33,17 +33,17 @@ import wave
 
 class AudioRecorder:
     def __init__(self, **params):
-        params.setdefault('format', pyaudio.paInt16)
-        params.setdefault('channels', 1)
-        params.setdefault('rate', 16000)
-        params.setdefault('frames_per_buffer', 1024)
+        params.setdefault("format", pyaudio.paInt16)
+        params.setdefault("channels", 1)
+        params.setdefault("rate", 16000)
+        params.setdefault("frames_per_buffer", 1024)
         self.audio = pyaudio.PyAudio()
         self.stream = self.audio.open(input=True, **params)
         self.params = params
         self.frames = []
 
     def update(self):
-        self.frames.append(self.stream.read(self.params['frames_per_buffer']))
+        self.frames.append(self.stream.read(self.params["frames_per_buffer"]))
 
     def stop(self):
         if not self.stream.is_stopped():
@@ -53,11 +53,11 @@ class AudioRecorder:
 
     def save(self, filename):
         self.stop()
-        with wave.open(filename, 'wb') as wf:
-            wf.setnchannels(self.params['channels'])
-            wf.setsampwidth(self.audio.get_sample_size(self.params['format']))
-            wf.setframerate(self.params['rate'])
-            wf.writeframes(b''.join(self.frames))
+        with wave.open(filename, "wb") as wf:
+            wf.setnchannels(self.params["channels"])
+            wf.setsampwidth(self.audio.get_sample_size(self.params["format"]))
+            wf.setframerate(self.params["rate"])
+            wf.writeframes(b"".join(self.frames))
 
 
 class ThreadedRecorder(Thread, AudioRecorder):
@@ -81,18 +81,18 @@ class ThreadedRecorder(Thread, AudioRecorder):
 class SupportSkill(MycroftSkill):
     # TODO: Will need to read from config under KDE, etc.
     log_locations = [
-        '/opt/mycroft/*.json',
-        '/var/log/mycroft/*.log',
-        '/etc/mycroft/*.conf',
-        join(dirname(dirname(mycroft.__file__)), 'scripts', 'logs', '*.log')
+        "/opt/mycroft/*.json",
+        "/var/log/mycroft/*.log",
+        "/etc/mycroft/*.conf",
+        join(dirname(dirname(mycroft.__file__)), "scripts", "logs", "*.log"),
     ]
-    log_types = ['audio', 'bus', 'enclosure', 'skills', 'update', 'voice']
+    log_types = ["audio", "bus", "enclosure", "skills", "update", "voice"]
 
     def get_log_files(self):
         log_files = sum([glob(pattern) for pattern in self.log_locations], [])
         for i in self.log_locations:
             for log_type in self.log_types:
-                fn = i.replace('*', log_type)
+                fn = i.replace("*", log_type)
                 if fn in log_files:
                     continue
                 if isfile(fn):
@@ -100,39 +100,39 @@ class SupportSkill(MycroftSkill):
         return log_files
 
     def create_debug_package(self, extra_files=None):
-        fd, name = mkstemp(suffix='.zip')
+        fd, name = mkstemp(suffix=".zip")
         tmp_folder = mkdtemp()
         zip_files = []
         for file in self.get_log_files() + (extra_files or []):
-            tar_name = file.strip('/').replace('/', '.')
+            tar_name = file.strip("/").replace("/", ".")
             tmp_file = join(tmp_folder, tar_name)
             shutil.copy(file, tmp_file)
             zip_files.append(tar_name)
 
         chdir(tmp_folder)
         try:
-            with ZipFile(name, 'w', ZIP_DEFLATED) as zf:
+            with ZipFile(name, "w", ZIP_DEFLATED) as zf:
                 for fn in zip_files:
                     zf.write(fn)
         except OSError as e:
-            self.log.warning('Failed to create debug package: {}'.format(e))
+            self.log.warning("Failed to create debug package: {}".format(e))
             return None
         return name
 
     def upload_file(self, filename):
-        with open(filename, 'rb') as f:
-            r = requests.post('https://0x0.st', files={'file': f})
+        with open(filename, "rb") as f:
+            r = requests.post("https://0x0.st", files={"file": f})
         if r.status_code != 200:
-            self.log.warning('Failed to post logs: {}'.format(r.text))
-            return ''
+            self.log.warning("Failed to post logs: {}".format(r.text))
+            return ""
         return r.text.strip()
 
     def get_device_name(self):
         try:
-            return DeviceApi().get()['name']
+            return DeviceApi().get()["name"]
         except:
-            self.log.exception('API Error')
-            return ':error:'
+            self.log.exception("API Error")
+            return ":error:"
 
     def upload_debug_package(self, extra_files=None):
         package_fn = self.create_debug_package(extra_files)
@@ -144,54 +144,52 @@ class SupportSkill(MycroftSkill):
         return url
 
     # "Create a support ticket"
-    @intent_handler('contact.support.intent')
+    @intent_handler("contact.support.intent")
     def troubleshoot(self):
         # Get a problem description from the user
-        user_words = self.get_response('confirm.support', num_retries=0)
+        user_words = self.get_response("confirm.support", num_retries=0)
 
-        yes_words = self.translate_list('yes')
+        yes_words = self.translate_list("yes")
 
         # TODO: .strip() shouldn't be needed, translate_list should remove
         #       the '\r' I'm seeing.  Remove after bugfix.
-        if (not user_words or not any(
-                i.strip() in user_words for i in yes_words
-        )):
-            self.speak_dialog('cancelled')
+        if not user_words or not any(i.strip() in user_words for i in yes_words):
+            self.speak_dialog("cancelled")
             return
 
-        sr = self.config_core['listener']['sample_rate']
+        sr = self.config_core["listener"]["sample_rate"]
         recorder = ThreadedRecorder(rate=sr)
-        description = self.get_response('ask.description', num_retries=0)
+        description = self.get_response("ask.description", num_retries=0)
         recorder.stop()
 
         if description is None:
-            self.speak_dialog('cancelled')
+            self.speak_dialog("cancelled")
             return
 
-        fd, audio_file = mkstemp(suffix='.wav')
+        fd, audio_file = mkstemp(suffix=".wav")
         recorder.save(audio_file)
 
-        self.speak_dialog('one.moment')
+        self.speak_dialog("one.moment")
 
         # Log so that the message will appear in the package of logs sent
-        self.log.debug("Troubleshooting Package Description: " +
-                       str(description))
+        self.log.debug("Troubleshooting Package Description: " + str(description))
 
         # Upload the logs to the web
         url = self.upload_debug_package([audio_file])
         if not url:
-            self.speak_dialog('upload.failed')
+            self.speak_dialog("upload.failed")
             return  # Something failed creating package. More info in logs
 
         # Create the troubleshooting email and send to user
         data = {
-            'url': url, 'device_name': self.get_device_name(),
-            'description': description
+            "url": url,
+            "device_name": self.get_device_name(),
+            "description": description,
         }
-        email = '\n'.join(self.translate_template('support.email', data))
-        title = self.translate('support.title')
+        email = "\n".join(self.translate_template("support.email", data))
+        title = self.translate("support.title")
         self.send_email(title, email)
-        self.speak_dialog('complete')
+        self.speak_dialog("complete")
 
 
 def create_skill():
